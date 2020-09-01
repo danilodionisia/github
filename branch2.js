4ba8bc5779f865011c61618e5c296539c946714f
Danilo Dionisia/INTELLIGENT SOLUTIONS/S2/Samsung Electronics <danilo.d@samsung.com>
2020-09-01 10:49:40 -0300

    commit com uma function

--- branch2.js
+++ branch2.js
@@ -1,2 +1,9 @@
 console.log('Branch 2');
 console.log('Teste de de git tag');
+var texto = 'esse é apenas um texto para ser impresso dentro da função';
+
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+imprimeTexto(texto);
